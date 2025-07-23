b68e216c643cf50a45de418b90423aa2c3ee9eed
lewys-miugo <lewiswambugu01@gmail.com>
2025-07-23 10:58:47 +0300

    Uploading files

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${params.id}`)
+      .then(r => r.json())
+      .then(data => setMovie(data));
+  }, [params.id]);
+
+  if (!movie.title) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>{movie.time}</p>
+      {movie.genres && movie.genres.map((genre, index) => (
+        <span key={index}>{genre}</span>
+      ))}
+    </div>
   );
-};
+}
 
 export default Movie;
